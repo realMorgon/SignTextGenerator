104bc440fe172ffee62cfdc54aa6843acbc523f0
Morgon <moritz.olthoff@gmail.com>
2025-02-12 21:33:44 +0100

    Initial commit

---
+++ src/main/java/io/github/realMorgon/signTextGenerator/SignTextGenerator.java
@@ -0,0 +1,17 @@
+package io.github.realMorgon.signTextGenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SignTextGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
